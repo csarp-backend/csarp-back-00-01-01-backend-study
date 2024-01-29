2a2f7e40cb019e472bd406e885e9cd388454b040
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 10:40:44 +0100

    KretaInMemoryContext tesztadat felvitel lehetőséggel

---
+++ Kreta.Backend/Context/KretaInMemoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Context
+{
+    public class KretaInMemoryContext : KretaContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+    }
+}
