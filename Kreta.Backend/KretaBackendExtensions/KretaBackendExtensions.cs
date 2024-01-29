29ff03095c27a4cc3f4cf94c63ee7c7bea067c1d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 10:07:56 +0100

    IP címek és cors beállítások

---
+++ Kreta.Backend/KretaBackendExtensions/KretaBackendExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Kreta.Backend.KretaBackendExtensions
+{
+    public static class KretaBackendExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+
+            services.AddCors(option =>
+                 option.AddPolicy(name: "KretaCors",
+                     policy =>
+                     {
+                         policy.WithOrigins("https://localhost:7020/")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+                     }
+                 )
+            );
+        }
+    }
+}
